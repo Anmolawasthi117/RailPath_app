fadd10eeb4031b9f0aa911dae0c7853e9d5c769a
Alpharex121 <arpitkoshta08@gmail.com>
2024-08-29 20:45:51 +0530

    fixed ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DummyMapComponent from './Components/DummyMapComponent';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DummyMapComponent from "./Components/DummyMapComponent";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<DummyMapComponent />} />
-        <Route path="/train-status" element={<DummyMapComponent selectedMap="Train status" />} />
-        <Route path="/ticket-counter" element={<DummyMapComponent selectedMap="Ticket counter" />} />
-        <Route path="/waiting-room" element={<DummyMapComponent selectedMap="Waiting Room" />} />
-        <Route path="/luggage-room" element={<DummyMapComponent selectedMap="Luggage Room" />} />
-        <Route path="/cafeteria" element={<DummyMapComponent selectedMap="Cafeteria" />} />
+        <Route
+          path="/wheelchair"
+          element={<DummyMapComponent selectedMap="wheelchair" />}
+        />
+        <Route
+          path="/ticket-counter"
+          element={<DummyMapComponent selectedMap="Ticket counter" />}
+        />
+        <Route
+          path="/waiting-room"
+          element={<DummyMapComponent selectedMap="Waiting Room" />}
+        />
+        <Route
+          path="/luggage-room"
+          element={<DummyMapComponent selectedMap="Luggage Room" />}
+        />
+        <Route
+          path="/cafeteria"
+          element={<DummyMapComponent selectedMap="Cafeteria" />}
+        />
       </Routes>
     </Router>
   );
--- src/Components/DummyMapComponent.jsx
+++ src/Components/DummyMapComponent.jsx
@@ -1,22 +1,24 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
-const DummyMapComponent = ({ selectedMap = 'default' }) => {
+const DummyMapComponent = ({ selectedMap = "default" }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
   const dropdownRef = useRef(null);
 
   const maps = {
-    default: 'https://media.discordapp.net/attachments/1071688106190831658/1278443526312693861/default_map.jpg?ex=66d17b91&is=66d02a11&hm=33299ff083ec883679e22aa863043f7c2347522946289fb588f1c2ea98fcf19d&=&format=webp&width=328&height=569',
-    'Train status': 'https://media.discordapp.net/attachments/1071688106190831658/1278443526547308685/cafeteria_map.jpg?ex=66d17b91&is=66d02a11&hm=be82484d5158be20ed93971033ed0f07b11d362e8c3066b5971001fda519ee4e&=&format=webp&width=305&height=570',
-    'Ticket counter': 'https://media.discordapp.net/attachments/1071688106190831658/1278443526811811982/ticket_map.jpg?ex=66d17b91&is=66d02a11&hm=ddc9f4392d73c0b44e9eaef8086723210bdb45468925612ed405a00a37b45424&=&format=webp&width=298&height=570',
-    'Waiting Room': 'https://media.discordapp.net/attachments/1071688106190831658/1278443527558402111/luggage_map.jpg?ex=66d17b91&is=66d02a11&hm=bc283d8cd8303b0f7d60bb14c387b7024d6c189cbf9a74af26c7eb333c8f1ed6&=&format=webp&width=309&height=570',
-    'Luggage Room': 'https://media.discordapp.net/attachments/1071688106190831658/1278443527306608711/waiting_map.jpg?ex=66d17b91&is=66d02a11&hm=a8f317c3520d38587581ca9a3024af29fad0b8d4be498f0af8cb83e45f42a197&=&format=webp&width=371&height=570',
-    'Cafeteria': 'https://media.discordapp.net/attachments/1071688106190831658/1278443527071731763/wheel_map.jpg?ex=66d17b91&is=66d02a11&hm=d60f98790d9143df5ed1c967d4453c49e2b14bb61f6da986b421a397163ae4cc&=&format=webp&width=416&height=570',
-  };
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    default:
+      "https://media.discordapp.net/attachments/1071688106190831658/1278443526312693861/default_map.jpg?ex=66d17b91&is=66d02a11&hm=33299ff083ec883679e22aa863043f7c2347522946289fb588f1c2ea98fcf19d&=&format=webp&width=328&height=595",
+    wheelchair:
+      "https://media.discordapp.net/attachments/1071688106190831658/1278443527071731763/wheel_map.jpg?ex=66d17b91&is=66d02a11&hm=d60f98790d9143df5ed1c967d4453c49e2b14bb61f6da986b421a397163ae4cc&=&format=webp&width=483&height=877",
+    "Ticket counter":
+      "https://media.discordapp.net/attachments/1071688106190831658/1278443526547308685/cafeteria_map.jpg?ex=66d17b91&is=66d02a11&hm=be82484d5158be20ed93971033ed0f07b11d362e8c3066b5971001fda519ee4e&=&format=webp&width=353&height=640",
+    "Waiting Room":
+      "https://media.discordapp.net/attachments/1071688106190831658/1278443527558402111/luggage_map.jpg?ex=66d17b91&is=66d02a11&hm=bc283d8cd8303b0f7d60bb14c387b7024d6c189cbf9a74af26c7eb333c8f1ed6&=&format=webp&width=360&height=652",
+    "Luggage Room":
+      "https://media.discordapp.net/attachments/1071688106190831658/1278443526811811982/ticket_map.jpg?ex=66d17b91&is=66d02a11&hm=ddc9f4392d73c0b44e9eaef8086723210bdb45468925612ed405a00a37b45424&=&format=webp&width=346&height=628",
+    Cafeteria:
+      "https://media.discordapp.net/attachments/1071688106190831658/1278443527306608711/waiting_map.jpg?ex=66d17b91&is=66d02a11&hm=a8f317c3520d38587581ca9a3024af29fad0b8d4be498f0af8cb83e45f42a197&=&format=webp&width=431&height=782",
   };
 
   const closeMenu = () => {
@@ -35,23 +37,26 @@ const DummyMapComponent = ({ selectedMap = 'default' }) => {
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
-  return (  
-    <div  className="flex flex-col max-h-screen bg-gray-100">
-     
-      <div className="bg-blue-500 text-white p-4 flex justify-between items-center relative shadow-md">
+  return (
+    <div className="flex flex-col h-[100vh] bg-gray-100">
+      <div className="bg-blue-500 text-white p-4  flex justify-between items-center h-[10%] shadow-md">
         <div className="text-lg font-bold">Rail Path</div>
 
-      
         <div className="relative">
-          <button onClick={toggleMenu} className="focus:outline-none">
+          <button
+            onClick={() => {
+              setIsMenuOpen(!isMenuOpen);
+            }}
+            className="focus:outline-none"
+          >
             <svg
-              className="h-6 w-6"
+              className="h-6 w-6 "
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -65,53 +70,98 @@ const DummyMapComponent = ({ selectedMap = 'default' }) => {
               />
             </svg>
           </button>
-
-        
           {isMenuOpen && (
             <div
               ref={dropdownRef}
               className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-lg z-50 transition-transform transform opacity-100"
-              style={{ transition: 'transform 0.3s ease-out, opacity 0.3s ease-out' }}
+              style={{
+                transition: "transform 0.3s ease-out, opacity 0.3s ease-out",
+              }}
             >
               <ul className="list-none text-black p-0 m-0">
-                <li className="p-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleMenuClick('Train status', '/train-status')}>Wheel chair collection</li>
-                <li className="p-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleMenuClick('Ticket counter', '/ticket-counter')}>Ticket Counter</li>
-                <li className="p-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleMenuClick('Waiting Room', '/waiting-room')}>Waiting Room</li>
-                <li className="p-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleMenuClick('Luggage Room', '/luggage-room')}>Luggage Room</li>
-                <li className="p-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleMenuClick('Cafeteria', '/cafeteria')}>Cafeteria</li>
+                <li
+                  className="p-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleMenuClick("default", "/")}
+                >
+                  Station Map
+                </li>
+                <li
+                  className="p-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleMenuClick("wheelchair", "/wheelchair")}
+                >
+                  Wheel chair collection
+                </li>
+                <li
+                  className="p-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() =>
+                    handleMenuClick("Ticket counter", "/ticket-counter")
+                  }
+                >
+                  Ticket Counter
+                </li>
+                <li
+                  className="p-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() =>
+                    handleMenuClick("Waiting Room", "/waiting-room")
+                  }
+                >
+                  Waiting Room
+                </li>
+                <li
+                  className="p-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() =>
+                    handleMenuClick("Luggage Room", "/luggage-room")
+                  }
+                >
+                  Luggage Room
+                </li>
+                <li
+                  className="p-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleMenuClick("Cafeteria", "/cafeteria")}
+                >
+                  Cafeteria
+                </li>
               </ul>
             </div>
           )}
         </div>
       </div>
 
-      
-      <div className="flex-1 bg-gray-300 relative">
+      <div className="flex h-[80%%] bg-gray-300   ">
         <img
           src={maps[selectedMap]}
           alt="Map"
-          className="w-full h-full object-contain"
+          className="w-full h-full object-fill"
           style={{
             zIndex: 1,
           }}
         />
 
-       
-        <div className="absolute top-4 right-4 flex flex-col space-y-2 z-10"> 
-  <button className="p-2 bg-white rounded-full shadow hover:bg-gray-200 flex justify-center items-center">
-    <img src="https://imgs.search.brave.com/RleXD8IHedCu7KRM40YHjmmHqfH2fnirQrrOoCe7VtQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni83MjY5LzcyNjk4/MzIucG5nP3NlbXQ9/YWlzX2h5YnJpZA" alt="" className='h-10 w-10' />
-  </button>
-  <button className="p-2 bg-white rounded-full shadow hover:bg-gray-200 flex justify-center items-center">
-    <img src="https://imgs.search.brave.com/HXaNdXES51uHN3peXeaVMMS_wJ7pHrNhxesOWFTPLj4/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4z/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvbW9iaWxlLXBo/b25lLTQxLzQwMC92/aWJyYXRpb24tMTI4/LnBuZw" alt="" className='h-10 w-10' />
-  </button>
-  <button className="p-2 bg-white rounded-full shadow hover:bg-gray-200 flex justify-center items-center">
-    <img src="https://imgs.search.brave.com/1uyX7zc8ElRuDtdfktZ5c-tOtP76FgdirLaAvXEDqqs/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4z/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvYmFzaWMtbmF2/aWdhdGlvbi0yLzI0/L3NyLWNhcHRpb24t/NjQucG5n" alt="" className='h-10 w-10' />
-  </button>
-</div>
-
+        <div className="absolute top-[10%] right-4 flex flex-col space-y-2 z-10">
+          <button className="p-2 bg-white rounded-full shadow hover:bg-gray-200 flex justify-center items-center">
+            <img
+              src="https://imgs.search.brave.com/RleXD8IHedCu7KRM40YHjmmHqfH2fnirQrrOoCe7VtQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni83MjY5LzcyNjk4/MzIucG5nP3NlbXQ9/YWlzX2h5YnJpZA"
+              alt=""
+              className="h-10 w-10"
+            />
+          </button>
+          <button className="p-2 bg-white rounded-full shadow hover:bg-gray-200 flex justify-center items-center">
+            <img
+              src="https://imgs.search.brave.com/HXaNdXES51uHN3peXeaVMMS_wJ7pHrNhxesOWFTPLj4/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4z/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvbW9iaWxlLXBo/b25lLTQxLzQwMC92/aWJyYXRpb24tMTI4/LnBuZw"
+              alt=""
+              className="h-10 w-10"
+            />
+          </button>
+          <button className="p-2 bg-white rounded-full shadow hover:bg-gray-200 flex justify-center items-center">
+            <img
+              src="https://imgs.search.brave.com/1uyX7zc8ElRuDtdfktZ5c-tOtP76FgdirLaAvXEDqqs/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4z/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvYmFzaWMtbmF2/aWdhdGlvbi0yLzI0/L3NyLWNhcHRpb24t/NjQucG5n"
+              alt=""
+              className="h-10 w-10"
+            />
+          </button>
+        </div>
       </div>
 
-    
       <div className="bg-white p-4 flex justify-between items-center border-t border-gray-200 shadow-md">
         <div className="text-sm text-gray-500">Explore nearby</div>
         <button className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-colors">
